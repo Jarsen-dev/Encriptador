928a8fb01bb6885084286eb4f9e6707db211fcee
Armando Desktop <joseaat.17@gmail.com>
2023-04-15 19:36:36 -0600

    Se añadio script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('.input');
+const encrypt = document.querySelector('.encrypt');
+const decrypt = document.querySelector('.decrypt');
+const output = document.querySelector('.output');
+const copy = document.querySelector('.copy-text');
+
+const encryptText = () => {
+    
+}
